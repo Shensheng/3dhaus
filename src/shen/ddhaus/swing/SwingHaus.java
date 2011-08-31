cb2d6466b1d95490fc66a6193b689efb42a19ca7
Mathias Kub <git@makubi.at>
2011-08-31 23:01:52 +0200

    Kommentare hinzugefuegt.

--- src/shen/ddhaus/swing/SwingHaus.java
+++ src/shen/ddhaus/swing/SwingHaus.java
@@ -2,12 +2,14 @@ package shen.ddhaus.swing;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class SwingHaus extends JFrame{
+public class SwingHaus extends JFrame implements ActionListener{
 	
 	private static final long serialVersionUID = 1L;
 	
@@ -15,21 +17,44 @@ public class SwingHaus extends JFrame{
 	private JButton okButton;
 
 	public SwingHaus() {
+		// Titel des Fensters
 		setTitle("Swing Haus");
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// Setze die Groesse und Position des Fensters
 		setFrameLocation(800, 600);
+		
+		// Verwendetes Layout
 		setLayout(new BorderLayout());
 		
+		// Initialisiere ein neues Label, in dem Text angezeigt werden kann
 		textLabel = new JLabel();
 		textLabel.setText("Willkommen! :-)");
 		
+		// Initialisiere neuen Button
 		okButton = new JButton("OK");
+		// Fuege Listener hinzu, damit eine Aktion beim Druecken des Buttons aufgefuehrt wird
+		// Siehe dazu Methode actionPerformed(ActionEvent)
+		okButton.addActionListener(this);
 		
-		
+		// Fuege Text zum Fenster hinzu und setze es in den Norden (oben)
 		add(textLabel, BorderLayout.NORTH);
+		// Fuege Button zum Fenster hinzu und setze es in den Sueden (unten)
 		add(okButton, BorderLayout.SOUTH);
 		
+		// Zeige Fenster an
 		setVisible(true);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object source = e.getSource();
+		
+		// Wenn der Button gedrueckt wird, beende das Programm
+		if(source == okButton) {
+			System.exit(0);
+		}
 	}
 	
 	private void setFrameLocation(int frameWidth, int frameHeight) {
@@ -42,5 +67,6 @@ public class SwingHaus extends JFrame{
 	public static void main(String[] args) {
 		new SwingHaus();
 	}
+	
 }
 
